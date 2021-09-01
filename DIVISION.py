619539f22f1e0403c1db4a122fb226ac31178ab7
Rafi <rafitanjibulhasan@gmail.com>
2021-09-02 01:42:24 +0600

    python basic

---
+++ DIVISION.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)  #the floor division // rounds the result down to the nearest whole number
+    print(a/b)
---
+++ HelloWorld.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__': #here__name__=='__main__' means the start of the program
+    print ("Hello, World!")
+
+
+'''All the built-ins in Python are functions, as is everything in the standard library. The rules for naming a function are a lot like rules for naming a variable: They must start with a letter or an underscore__.They should be lowercase.They can't be the same as a Python keyword'''
